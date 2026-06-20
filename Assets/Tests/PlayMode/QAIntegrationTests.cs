c4ecbbac714e388429d29a769348d413ffbef2a0
theDawckta <lasaterz@gmail.com>
2026-06-20 12:04:40 -0700

    fix: CS1513 typo in QAIntegrationTests -- duplicate ); on line 16

---
+++ Assets/Tests/PlayMode/QAIntegrationTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+[TestFixture]
+public class QAIntegrationTests
+{
+    [UnityTest]
+    public IEnumerator StartScreen_IsVisible_OnLoad()
+    {
+        yield return SceneManager.LoadSceneAsync("Main");
+        yield return new WaitForSeconds(1f);
+        Assert.IsNotNull(Object.FindAnyObjectByType<StartScreen>());
+    }
+}
